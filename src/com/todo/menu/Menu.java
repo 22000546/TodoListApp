9dc3e88ebb3d04b62e520cd13afc0957c2d28098
Yeeun <22000546@handong.edu>
2021-09-24 10:33:57 +0900

    Add menu

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -9,12 +9,14 @@ public class Menu {
         System.out.println("2. 기존 항목 삭제 : del");
         System.out.println("3. 기존 항목 수정 : edit");
         System.out.println("4. 전체 항목 보기 : ls");
-        System.out.println("5. 이름 순으로 정렬 : ls_name_asc");
-        System.out.println("6. 이름 역순으로 정렬 : ls_name_desc");
-        System.out.println("7. 시간 순으로 정렬 : ls_date");
-        System.out.println("8. 시간 역순으로 정렬 : ls_date_desc");
-        System.out.println("9. 키워드로 검색 : find <keyword>");
-        System.out.println("10. 종료 : exit (Or press escape key)");
+        System.out.println("5. 카테고리 목록 보기 : ls_cate");
+        System.out.println("6. 이름 순으로 정렬 : ls_name_asc");
+        System.out.println("7. 이름 역순으로 정렬 : ls_name_desc");
+        System.out.println("8. 시간 순으로 정렬 : ls_date");
+        System.out.println("9. 시간 역순으로 정렬 : ls_date_desc");
+        System.out.println("10. 키워드로 검색 : find <keyword>");
+        System.out.println("11. 카테고리 키워드로 검색 : find_cate <keyword>");
+        System.out.println("12. 종료 : exit (Or press escape key)");
         
     }
     
